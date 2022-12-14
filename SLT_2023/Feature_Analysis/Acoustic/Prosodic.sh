14633414baeba989e035f88cec7aff025a32ca07
annafavaro <anna.favaro96@gmail.com>
2022-12-14 17:32:21 -0500

    first

--- SLT_2023/Feature_Analysis/Acoustic/Prosodic.sh
+++ SLT_2023/Feature_Analysis/Acoustic/Prosodic.sh
@@ -5,16 +5,12 @@ export PATH=$PATH:$KALDI_ROOT/src/featbin/
 
 # prosodic features extraction using Disvoice: https://github.com/jcvasquezc/DisVoice/tree/master/disvoice/prosody
 
-
 python prosody.py "/export/c12/afavaro/NLS_Data_Set_16k" "/export/b14/afavaro/Acoustic_Features/prosody.csv" "true" "false" "csv"
 
-#python prosody.py "/export/b15/afavaro/Frontiers/ADR_2021/audio_16k/" "/export/b15/afavaro/Frontiers/ADR_2021/prosody.csv" "true" "false" "csv"
-
-
+python prosody.py "/export/b15/afavaro/Frontiers/ADR_2021/audio_16k/" "/export/b15/afavaro/Frontiers/ADR_2021/prosody.csv" "true" "false" "csv"
 
 
 # Pause-related features extraction using DigiPsych Prosody: https://github.com/NeuroLexDiagnostics/DigiPsych_Prosody
-
 
 
 python featurize.py -a  /export/c12/afavaro/NLS_Data_Set_16k -f 20
