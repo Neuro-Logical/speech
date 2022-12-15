fd9c4fab08a815a62707ef01a6919a6413876825
annafavaro <anna.favaro96@gmail.com>
2022-12-15 11:04:37 -0500

    first

--- Multilingual Evaluation of Interpretable Biomarkers/Data_Preprocessing/convert_to_16k.sh
+++ Multilingual Evaluation of Interpretable Biomarkers/Data_Preprocessing/convert_to_16k.sh
@@ -1,6 +1,8 @@
 in_dir=/export/c12/afavaro/NLS_Data_Set
 out_dir=/export/b15/afavaro/Frontiers/NLS/NLS_RESAMPLED/
 
+# Resampling of recordings to 16 kHz.
+
 for audio_File in $in_dir/*.wav
 do
 	uttID=$(basename "$audio_File")
