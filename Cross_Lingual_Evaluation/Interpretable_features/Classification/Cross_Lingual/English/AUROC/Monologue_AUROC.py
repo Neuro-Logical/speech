8a13431ce520b2e85082a5f0fb2d266636fc8c34
annafavaro <anna.favaro96@gmail.com>
2023-01-02 17:16:42 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/Classification/Cross_Lingual/English/AUROC/Monologue_AUROC.py
+++ Cross_Lingual_Evaluation/Interpretable_features/Classification/Cross_Lingual/English/AUROC/Monologue_AUROC.py
@@ -1,5 +1,8 @@
 BASE_DIR = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/"
 
+
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Cross_Lingual.Data_Prep_monologue import *
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Cross_Lingual.Utils_monologue import *
 import numpy as np
 import os
 from sklearn.ensemble import ExtraTreesClassifier
@@ -14,11 +17,11 @@ from Cross_Lingual.Data_Prep_monologue import *
 from sklearn.metrics import roc_auc_score
 np.random.seed(20)
 
-nls, nls_cols = nls_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/NLS/total_new_training.csv")
-colombian, colombian_cols = gita_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GITA/total_data_frame_novel_task_combined_ling_tot.csv")
-spain, spain_cols = neurovoz_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/NEUROVOZ/tot_data_experiments.csv")
-german, german_cols = german_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GERMAN/final_data_frame_with_intensity.csv")
-czech, czech_clols = czech_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/Czech/final_data_experiments_updated.csv")
+nls, nls_cols = nls_prep(os.path.join(BASE_DIR, "NLS/total_new_training.csv"))
+colombian, colombian_cols = gita_prep(os.path.join(BASE_DIR, "GITA/total_data_frame_novel_task_combined_ling_tot.csv"))
+spain, spain_cols = neurovoz_prep(os.path.join(BASE_DIR,"NEUROVOZ/tot_data_experiments.csv"))
+german, german_cols = german_prep(os.path.join(BASE_DIR, "GERMAN/final_data_frame_with_intensity.csv"))
+czech, czech_clols = czech_prep(os.path.join(BASE_DIR,"Czech/final_data_experiments_updated.csv"))
 
 one_inter = IntersecOfSets(german_cols, nls_cols, spain_cols)
 lista_to_keep = IntersecOfSets(one_inter, colombian_cols, czech_clols)
