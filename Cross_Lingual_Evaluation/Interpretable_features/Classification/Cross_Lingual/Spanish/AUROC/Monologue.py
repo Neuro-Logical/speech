f1bc8d8fe26d09e201c7aca89062ce4ac44dcf1c
annafavaro <anna.favaro96@gmail.com>
2023-01-02 17:43:19 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/Classification/Cross_Lingual/Spanish/AUROC/Monologue.py
+++ Cross_Lingual_Evaluation/Interpretable_features/Classification/Cross_Lingual/Spanish/AUROC/Monologue.py
@@ -1,18 +1,8 @@
-from sklearn.metrics import classification_report, confusion_matrix
-import sys
-sys.path.append("/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Multi_Lingual_PD/")
-from sklearn.datasets import load_iris
-import pandas as pd
-from sklearn.model_selection import train_test_split
+BASE_DIR = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/"
+
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Cross_Lingual.Data_Prep_monologue import *
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Cross_Lingual.Utils_monologue import *
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-from sklearn.inspection import DecisionBoundaryDisplay
-from sklearn.utils import shuffle
-import random
 import os
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
@@ -20,44 +10,25 @@ from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.feature_selection import SelectKBest, f_regression, f_classif
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import Pipeline
-from sklearn.datasets import make_classification
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_selection import SelectKBest, mutual_info_classif
-from Cross_Validation_Cross_Mean.Utils_monologue import *
-from Cross_Validation_Cross_Mean.Data_Prep_monologue import *
-from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 np.random.seed(20)
 
-nls, nls_cols = nls_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/NLS/total_new_training.csv")
-colombian, colombian_cols = gita_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GITA/total_data_frame_novel_task_combined_ling_tot.csv")
-spain, spain_cols = neurovoz_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/NEUROVOZ/tot_data_experiments.csv")
-german, german_cols = german_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GERMAN/final_data_frame_with_intensity.csv")
-czech, czech_clols = czech_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/Czech/final_data_experiments_updated.csv")
+
+nls, nls_cols = nls_prep(os.path.join(BASE_DIR, "NLS/total_new_training.csv"))
+colombian, colombian_cols = gita_prep(os.path.join(BASE_DIR, "GITA/total_data_frame_novel_task_combined_ling_tot.csv"))
+spain, spain_cols = neurovoz_prep(os.path.join(BASE_DIR,"NEUROVOZ/tot_data_experiments.csv"))
+german, german_cols = german_prep(os.path.join(BASE_DIR, "GERMAN/final_data_frame_with_intensity.csv"))
+czech, czech_cols = czech_prep(os.path.join(BASE_DIR,"Czech/final_data_experiments_updated.csv"))
 
 one_inter = IntersecOfSets(german_cols, nls_cols, spain_cols)
-lista_to_keep = IntersecOfSets(one_inter, colombian_cols, czech_clols)
+list_to_keep = IntersecOfSets(one_inter, colombian_cols, czech_cols)
 
-nls = nls[nls.columns.intersection(lista_to_keep)]
-czech = czech[czech.columns.intersection(lista_to_keep)]
-colombian = colombian[colombian.columns.intersection(lista_to_keep)]
-german = german[german.columns.intersection(lista_to_keep)]
-spain = spain[spain.columns.intersection(lista_to_keep)]
+nls = nls[nls.columns.intersection(list_to_keep)]
+czech = czech[czech.columns.intersection(list_to_keep)]
+colombian = colombian[colombian.columns.intersection(list_to_keep)]
+german = german[german.columns.intersection(list_to_keep)]
+spain = spain[spain.columns.intersection(list_to_keep)]
 
 colombian = colombian.reindex(sorted(colombian.columns), axis=1)
 german = german.reindex(sorted(german.columns), axis=1)
