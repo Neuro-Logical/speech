e1a939ab65211a93cfdd10b182357859b3cd7596
annafavaro <anna.favaro96@gmail.com>
2023-01-03 12:06:23 -0500

    first

--- Cross_Lingual_Evaluation/Data_Preprocessing/Extract_Transcripts.py
+++ Cross_Lingual_Evaluation/Data_Preprocessing/Extract_Transcripts.py
@@ -3,28 +3,26 @@ import pandas as pd
 import whisper
 pd.options.display.max_rows = 100
 
-repo = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/'
-output_folder = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_Transcripts_2'
-paths = [os.path.join(repo, base) for base in os.listdir(repo)]
+def extract_speech_transcripts(path_to_recordings, output_folder):
 
-# keep non empty recordings
-files = []
-for m in paths:
-    size = os.stat(m).st_size/1000
-    if size > 56:
-        files.append(m)
+    paths = [os.path.join(path_to_recordings, base) for base in os.listdir(path_to_recordings)]
 
-# extract transcripts and saved them
-for i in files:
-    #print(i)
-    model = whisper.load_model("medium")
-    result = model.transcribe(i)
-    test = result['text']
-    base = os.path.basename(i).split(".wav")[0]
-    total = os.path.join(output_folder, base + ".txt")
-    text_file = open(total, "wt")
-    n = text_file.write(test)
-    text_file.close()
-                                                                    
+    files = []
+    for m in paths:
+        size = os.stat(m).st_size/1000
+        if size > 56:
+            files.append(m)
+
+    for i in files:
+        #print(i)
+        model = whisper.load_model("medium")
+        result = model.transcribe(i)
+        test = result['text']
+        base = os.path.basename(i).split(".wav")[0]
+        total = os.path.join(output_folder, base + ".txt")
+        text_file = open(total, "wt")
+        n = text_file.write(test)
+        text_file.close()
+
     
 
