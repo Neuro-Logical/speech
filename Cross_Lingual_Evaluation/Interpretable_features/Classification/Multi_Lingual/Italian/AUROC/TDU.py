2f0fb5da470b34ba92365a0158ab70ecce6eba3d
annafavaro <anna.favaro96@gmail.com>
2023-01-03 09:40:01 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/Classification/Multi_Lingual/Italian/AUROC/TDU.py
+++ Cross_Lingual_Evaluation/Interpretable_features/Classification/Multi_Lingual/Italian/AUROC/TDU.py
@@ -1,5 +1,7 @@
-import sys
-sys.path.append("/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Lingual_Evaluation/")
+BASE = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis"
+
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Multi_Lingual.Data_Prep_TDU import *
+from Cross_Lingual_Evaluation.Interpretable_features.Classification.Multi_Lingual.Utils_TDU import *
 import numpy as np
 import random
 import os
@@ -10,15 +12,13 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from Cross_Validation_Multi_Lingual.Utils_TDU import *
-from Cross_Validation_Multi_Lingual.Data_Prep_TDU import *
 from sklearn.metrics import roc_auc_score
 random.seed(10)
 
-colombian, colombian_cols = gita_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GITA/total_data_frame_novel_task_combined_ling_tot.csv")
-german, german_cols = german_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/GERMAN/final_data_frame_with_intensity.csv")
-italian, italian_cols = italian_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/ITALIAN_PD/tot_experiments_ling_fin.csv")
-spain, spain_cols = neurovoz_prep("/export/b15/afavaro/Frontiers/submission/Statistical_Analysis/NEUROVOZ/tot_data_experiments.csv")
+colombian, colombian_cols = gita_prep(os.path.join(BASE, "/GITA/total_data_frame_novel_task_combined_ling_tot.csv"))
+german, german_cols = german_prep(os.path.join(BASE, "/GERMAN/final_data_frame_with_intensity.csv"))
+italian, italian_cols = italian_prep(os.path.join(BASE, "/ITALIAN_PD/tot_experiments_ling_fin.csv"))
+spain, spain_cols = neurovoz_prep(os.path.join(BASE, "/NEUROVOZ/tot_data_experiments.csv"))
 
 one_inter = IntersecOftwo(german_cols, italian_cols)
 lista_to_keep = IntersecOfSets(one_inter, colombian_cols,spain_cols)
