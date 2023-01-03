a8dd7e883db60b308b9084b44b52c82543cff0bb
annafavaro <anna.favaro96@gmail.com>
2023-01-03 12:10:47 -0500

    first

--- Cross_Lingual_Evaluation/Data_Preprocessing/Extract_Transcripts.py
+++ Cross_Lingual_Evaluation/Data_Preprocessing/Extract_Transcripts.py
@@ -1,23 +1,21 @@
 import os
-import pandas as pd
 import whisper
-pd.options.display.max_rows = 100
+
 
 def extract_speech_transcripts(path_to_recordings, output_folder):
-
     """Extract speech transcripts"""
     paths = [os.path.join(path_to_recordings, base) for base in os.listdir(path_to_recordings)]
 
     # keep only non-empty recordings (> 56 Bytes)
     files = []
     for m in paths:
-        size = os.stat(m).st_size/1000
+        size = os.stat(m).st_size / 1000
         if size > 56:
             files.append(m)
 
     # extract and save transcripts.
     for i in files:
-        #print(i)
+        # print(i)
         model = whisper.load_model("medium")
         result = model.transcribe(i)
         test = result['text']
@@ -26,6 +24,3 @@ def extract_speech_transcripts(path_to_recordings, output_folder):
         text_file = open(total, "wt")
         n = text_file.write(test)
         text_file.close()
-
-    
-
