1920f975caa813199eaf9422accf74138f2d8ba7
annafavaro <anna.favaro96@gmail.com>
2023-01-04 17:23:14 -0500

    first

--- SLT_2023/Data_Preprocessing/convert_to_16k.sh
+++ SLT_2023/Data_Preprocessing/convert_to_16k.sh
@@ -3,6 +3,9 @@
 in_dir=/export/c12/afavaro/NLS_Data_Set
 out_dir=/export/c12/afavaro/NLS_Data_Set_16k
 
+## in_dir: directory containing recordings to be converted
+## out_dir: output directory containing recordings converted to 16 kHz.
+
 for audio_File in $in_dir/*.wav
 do
 	uttID=$(basename "$audio_File")
--- SLT_2023/Feature_Extraction/Acoustic/Prosodic.sh
+++ SLT_2023/Feature_Extraction/Acoustic/Prosodic.sh
@@ -4,18 +4,16 @@ export PATH=$PATH:$KALDI_ROOT/src/featbin/
 
 # prosodic features extraction using Disvoice: https://github.com/jcvasquezc/DisVoice/tree/master/disvoice/prosody
 # The script saves a feature matrix in csv format.
+# # This script should be run inside the folder named: disvoice/prosody.
 
 python prosody.py "/export/c12/afavaro/NLS_Data_Set_16k" "/export/b14/afavaro/Acoustic_Features/prosody.csv" "true" "false" "csv"
-
 python prosody.py "/export/b15/afavaro/Frontiers/ADR_2021/audio_16k/" "/export/b15/afavaro/Frontiers/ADR_2021/prosody.csv" "true" "false" "csv"
 
-
+##############################################################################################################################################
 # Pause related features extracted with: https://github.com/NeuroLexDiagnostics/DigiPsych_Prosody
-
 # f = frame length, namely the length of the frame used by the VAD adopted for the feature extraction.
 # f can be 10, 20, 30 ms.
 # -a is the path to the folder containing the recordings.
-
+# This script should be run inside the folder named: DigiPsych_Prosody.
 python featurize.py -a  /export/c12/afavaro/NLS_Data_Set_16k -f 20
-
 python featurize.py -a  /export/b15/afavaro/Frontiers/ADR_2021/audio_16k/ -f 20
