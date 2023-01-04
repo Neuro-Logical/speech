6c307092b459ef99805c8594cd85cce0e83d811c
annafavaro <anna.favaro96@gmail.com>
2023-01-04 09:08:18 -0500

    first

--- Cross_Lingual_Evaluation/interpretable_features/nested_cross_validation/Mono_Lingual/Italian/SEN/TDU.py
+++ Cross_Lingual_Evaluation/interpretable_features/nested_cross_validation/Mono_Lingual/Italian/SEN/TDU.py
@@ -1,4 +1,6 @@
 BASE = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis"
+SPEC_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Val_Results_2/ITALIAN/RP/SPEC/'
+SENS_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Val_Results_2/ITALIAN/RP/SENS/'
 
 from Cross_Lingual_Evaluation.interpretable_features.nested_cross_validation.Mono_Lingual.Data_Prep_TDU import *
 from Cross_Lingual_Evaluation.interpretable_features.nested_cross_validation.Mono_Lingual.Utils import *
@@ -65,13 +67,9 @@ for i in range(1, 11):
     print('Sensitivity : ', sensitivity)
     specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
     print('spec : ', specificity)
-    SPEC = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Val_Results_2/ITALIAN/TDU/SPEC/'
-    SENS = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Val_Results_2/ITALIAN/TDU/SENS/'
-
-    with open(os.path.join(SPEC, f"SVM_spec_{i}.txt"), 'w') as f:
+    with open(os.path.join(SPEC_OUT_PATH, f"SVM_spec_{i}.txt"), 'w') as f:
         f.writelines(str(specificity))
-    #
-    with open(os.path.join(SENS, f"SVM_sens_{i}.txt"), 'w') as f:
+    with open(os.path.join(SENS_OUT_PATH, f"SVM_sens_{i}.txt"), 'w') as f:
         f.writelines(str(sensitivity))
 
    # KNeighborsClassifier
@@ -83,11 +81,9 @@ for i in range(1, 11):
     print('Sensitivity : ', sensitivity)
     specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
     print('spec : ', specificity)
-
-    with open(os.path.join(SPEC, f"KNN_spec_{i}.txt"), 'w') as f:
+    with open(os.path.join(SPEC_OUT_PATH, f"KNN_spec_{i}.txt"), 'w') as f:
         f.writelines(str(specificity))
-
-    with open(os.path.join(SENS, f"KNN_sens_{i}.txt"), 'w') as f:
+    with open(os.path.join(SENS_OUT_PATH, f"KNN_sens_{i}.txt"), 'w') as f:
         f.writelines(str(sensitivity))
 
     #RandomForestClassifier
@@ -99,11 +95,9 @@ for i in range(1, 11):
     print('Sensitivity : ', sensitivity)
     specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
     print('spec : ', specificity)
-
-    with open(os.path.join(SPEC, f"RF_spec_{i}.txt"), 'w') as f:
+    with open(os.path.join(SPEC_OUT_PATH, f"RF_spec_{i}.txt"), 'w') as f:
         f.writelines(str(specificity))
-
-    with open(os.path.join(SENS, f"RF_sens_{i}.txt"), 'w') as f:
+    with open(os.path.join(SENS_OUT_PATH, f"RF_sens_{i}.txt"), 'w') as f:
         f.writelines(str(sensitivity))
 
     # GradientBoostingClassifier
@@ -115,10 +109,8 @@ for i in range(1, 11):
     print('Sensitivity : ', sensitivity)
     specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
     print('spec : ', specificity)
-
-    with open(os.path.join(SPEC, f"XG_spec_{i}.txt"), 'w') as f:
+    with open(os.path.join(SPEC_OUT_PATH, f"XG_spec_{i}.txt"), 'w') as f:
         f.writelines(str(specificity))
-    #
     with open(os.path.join(SENS, f"XG_sens_{i}.txt"), 'w') as f:
         f.writelines(str(sensitivity))
 
@@ -131,11 +123,9 @@ for i in range(1, 11):
     print('Sensitivity : ', sensitivity)
     specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
     print('spec : ', specificity)
-
-    with open(os.path.join(SPEC, f"BAGG_spec_{i}.txt"), 'w') as f:
+    with open(os.path.join(SPEC_OUT_PATH, f"BAGG_spec_{i}.txt"), 'w') as f:
         f.writelines(str(specificity))
-
-    with open(os.path.join(SENS, f"BAGG_sens_{i}.txt"), 'w') as f:
+    with open(os.path.join(SENS_OUT_PATH, f"BAGG_sens_{i}.txt"), 'w') as f:
         f.writelines(str(sensitivity))
 
 
