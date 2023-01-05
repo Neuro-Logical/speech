3257316efc20ec72aa31bcad671b615cd19475db
annafavaro <anna.favaro96@gmail.com>
2023-01-05 15:41:36 -0500

    first

---
+++ Multilingual_Evaluation_of_Interpretable_Biomarkers/Feature_Extraction/whisperix_aligner.py
@@ -0,0 +1,25 @@
+import os.path
+
+import whisperx
+BASE = "/export/b15/afavaro/Frontiers/Italian_PD/Audio_Whole_Italian/"
+OUT_PATH = "/export/b15/afavaro/Frontiers/Italian_PD/alignment_whisperix"
+device = "cpu"
+model = whisperx.load_model("medium", device)
+
+audios = [os.path.join(BASE, elem) for elem in os.listdir(BASE)]
+
+for audio in audios:
+    text =[]
+    time_stamps = []
+    base_name = os.path.basename(audio_file).split("wav")
+    result = model.transcribe(audio_file)
+
+# load alignment model and metadata
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file, device)\
+    out = (result_aligned["word_segments"]) # after alignment
+    for element in out:
+        text.append(element['text'])
+        time_stamps.append(element['start'])
+    data = pd.DataFrame({'word': text, 'time_stamps': time_stamps})
+    data.to_csv(os.path.join(OUT_PATH, base_name + ".csv"))
