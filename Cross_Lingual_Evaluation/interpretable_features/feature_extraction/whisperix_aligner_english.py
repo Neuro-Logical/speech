dea0a6cbe121259f4fd0ebe30a415b1ee7133240
annafavaro <anna.favaro96@gmail.com>
2023-01-05 19:54:33 -0500

    first

--- Cross_Lingual_Evaluation/interpretable_features/feature_extraction/whisperix_aligner_english.py
+++ Cross_Lingual_Evaluation/interpretable_features/feature_extraction/whisperix_aligner_english.py
@@ -12,8 +12,8 @@ device = "cpu"
 model = whisperx.load_model("medium", device)
 
 audios = [os.path.join(BASE, elem) for elem in os.listdir(BASE)]
-
-for audio in audios:
+indx = audios.index("/export/b15/afavaro/Frontiers/NLS/NLS_RESAMPLED/NLS_105_ses1_Joke.wav")
+for audio in audios[indx:]:
     text =[]
     time_stamps = []
     base_name = os.path.basename(audio).split(".wav")[0]
