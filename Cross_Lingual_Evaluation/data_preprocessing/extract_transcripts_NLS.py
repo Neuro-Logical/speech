8edaf5685f7f0c62793ae816a46c0fef55f237d3
annafavaro <anna.favaro96@gmail.com>
2023-01-07 13:01:59 -0500

    first

---
+++ Cross_Lingual_Evaluation/data_preprocessing/extract_transcripts_NLS.py
@@ -0,0 +1,28 @@
+import os
+import whisper
+
+
+base ='/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k'
+output_folder = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_Transcripts/'
+
+paths = [os.path.join(path_to_recordings, base) for base in os.listdir(path_to_recordings)]
+
+# keep only non-empty recordings (> 56 Bytes)
+files = []
+for m in paths:
+    size = os.stat(m).st_size / 1000
+    if size > 56:
+        if "AD_006" in m:
+            files.append(m)
+
+# extract and save transcripts.
+for i in files:
+    # print(i)
+    model = whisper.load_model("medium")
+    result = model.transcribe(i)
+    test = result['text']
+    base = os.path.basename(i).split(".wav")[0]
+    total = os.path.join(output_folder, base + ".txt")
+    text_file = open(total, "wt")
+    n = text_file.write(test)
+    text_file.close()
