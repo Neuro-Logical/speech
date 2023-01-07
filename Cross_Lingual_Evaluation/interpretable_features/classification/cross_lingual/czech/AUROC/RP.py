c129a111da04dd3a21677fb8fe4824c7599c67d6
annafavaro <anna.favaro96@gmail.com>
2023-01-07 14:02:14 -0500

    first

---
+++ Cross_Lingual_Evaluation/interpretable_features/classification/cross_lingual/czech/AUROC/RP.py
@@ -0,0 +1,116 @@
+BASE_DIR = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis"
+OUT_PATH ='/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Cross_Val_Results_Cross_Mean1/CZECH/RP/AUROC/'
+
+import sys
+sys.path.append("/export/b15/afavaro/git_code_version/speech")
+from Cross_Lingual_Evaluation.interpretable_features.classification.cross_lingual.Data_Prep_RP import *
+from Cross_Lingual_Evaluation.interpretable_features.classification.cross_lingual.Utils_RP import *
+import numpy as np
+import os
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import roc_auc_score
+np.random.seed(20)
+
+nls, nls_cols = nls_prep(os.path.join(BASE_DIR, "NLS/Data_frame_RP.csv"))
+colombian, colombian_cols = gita_prep(os.path.join(BASE_DIR,"GITA/total_data_frame_novel_task_combined_ling_tot.csv"))
+german, german_cols = german_prep(os.path.join(BASE_DIR, "GERMAN/final_data_frame_with_intensity.csv"))
+czech, czech_cols = czech_prep(os.path.join(BASE_DIR, "Czech/final_data_experiments_updated.csv"))
+italian, italian_cols = italian_prep(os.path.join(BASE_DIR, "ITALIAN_PD/RP_data_frame.csv"))
+
+one_inter = IntersecOfSets(german_cols, nls_cols, italian_cols)
+lista_to_keep = IntersecOfSets(one_inter, colombian_cols, czech_cols)
+
+nls = nls[nls.columns.intersection(lista_to_keep)]
+czech = czech[czech.columns.intersection(lista_to_keep)]
+colombian = colombian[colombian.columns.intersection(lista_to_keep)]
+german = german[german.columns.intersection(lista_to_keep)]
+italian = italian[italian.columns.intersection(lista_to_keep)]
+
+colombian = colombian.reindex(sorted(colombian.columns), axis=1)
+german = german.reindex(sorted(german.columns), axis=1)
+nls = nls.reindex(sorted(nls.columns), axis=1)
+czech = czech.reindex(sorted(czech.columns), axis=1)
+italian = italian.reindex(sorted(italian.columns), axis=1)
+
+nls = preprocess_data_frame(nls)
+german = preprocess_data_frame(german)
+czech = preprocess_data_frame(czech)
+italian = preprocess_data_frame(italian)
+colombian = preprocess_data_frame(colombian)
+
+normalized_train_X_italian, y_train_italian, mean_italian, std_italian = normalize(italian)
+normalized_train_X_german, y_train_german, mean_german, std_german = normalize(german)
+normalized_train_X_nls, y_train_nls, mean_nls, std_nls = normalize(nls)
+normalized_train_X_colombian, y_train_colombian, mean_colombian, std_colombian = normalize(colombian)
+means = np.mean(np.stack([mean_italian, mean_german, mean_nls, mean_colombian], axis=1), axis=1)
+stds = np.mean(np.stack([std_italian, std_german, std_nls, std_colombian], axis=1), axis=1)
+
+normalized_train_X_czech, y_train_czech = normalize_test(czech, means, stds)
+
+training_data, training_labels = train_split(normalized_train_X_italian, y_train_italian, normalized_train_X_colombian,
+                                             y_train_colombian,
+                                             normalized_train_X_german, y_train_german, normalized_train_X_nls,
+                                             y_train_nls)
+
+test_data, test_labels = test_split(normalized_train_X_czech, y_train_czech)
+
+clf = ExtraTreesClassifier(n_estimators=30)
+clf = clf.fit(training_data, training_labels)
+model = SelectFromModel(clf, prefit=True, max_features=40)
+X_train = model.transform(training_data)
+cols = model.get_support(indices=True)
+X_test = test_data[:, cols]
+
+# SVM
+model = SVC(C=1.0, gamma= 0.01, kernel= 'rbf', probability=True)
+grid_result = model.fit(X_train, training_labels)
+grid_predictions = grid_result.predict_proba(X_test)
+grid_predictions = grid_predictions[:, 1]
+lr_auc = roc_auc_score(test_labels, grid_predictions)
+print(f"auroc is {lr_auc}")
+with open(os.path.join(OUT_PATH, f"SVM_AUROC.txt"), 'w') as f:
+    f.writelines(str(lr_auc))
+
+#model = KNeighborsClassifier()
+model = KNeighborsClassifier(metric='euclidean', n_neighbors=11, weights='distance')
+grid_result = model.fit(X_train, training_labels)
+grid_predictions = grid_result.predict_proba(X_test)
+grid_predictions = grid_predictions[:, 1]
+lr_auc = roc_auc_score(test_labels, grid_predictions)
+with open(os.path.join(OUT_PATH, f"KNN_AUROC.txt"), 'w') as f:
+    f.writelines(str(lr_auc))
+
+#model = RandomForestClassifier()
+model = RandomForestClassifier(max_features= 'sqrt', n_estimators= 1000)
+grid_result = model.fit(X_train, training_labels)
+grid_predictions = grid_result.predict_proba(X_test)
+grid_predictions = grid_predictions[:, 1]
+lr_auc = roc_auc_score(test_labels, grid_predictions)
+with open(os.path.join(OUT_PATH, f"RF_AUROC.txt"), 'w') as f:
+    f.writelines(str(lr_auc))
+
+# GradientBoostingClassifier
+model = GradientBoostingClassifier(learning_rate=0.1, max_depth=3, n_estimators=1000, subsample=0.7)
+grid_result = model.fit(X_train, training_labels)
+grid_predictions = grid_result.predict_proba(X_test)
+grid_predictions = grid_predictions[:, 1]
+lr_auc = roc_auc_score(test_labels, grid_predictions)
+print(f"auroc is {lr_auc}")
+with open(os.path.join(OUT_PATH, f"XGBoost_AUROC.txt"), 'w') as f:
+    f.writelines(str(lr_auc))
+
+# BaggingClassifier
+model = BaggingClassifier(n_estimators=1000, max_samples=0.5)
+grid_result = model.fit(X_train, training_labels)
+grid_predictions = grid_result.predict_proba(X_test)
+grid_predictions = grid_predictions[:, 1]
+lr_auc = roc_auc_score(test_labels, grid_predictions)
+print(f"auroc is {lr_auc}")
+with open(os.path.join(OUT_PATH, f"Bagging_AUROC.txt"), 'w') as f:
+        f.writelines(str(lr_auc))
---
+++ Cross_Lingual_Evaluation/interpretable_features/grid_search/mono_lingual/colombian/RP.py
@@ -0,0 +1,220 @@
+BASE = "/export/b15/afavaro/Frontiers/submission/Statistical_Analysis"
+SVM_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Best_hyperpameters_2/GITA/RP/SVM/RP.txt'
+KNN_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Best_hyperpameters_2/GITA/RP/KNN/RP.txt'
+RF_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Best_hyperpameters_2/GITA/RP/RF/RP.txt'
+XG_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Best_hyperpameters_2/GITA/RP/XG/RP.txt'
+BAGG_OUT_PATH = '/export/b15/afavaro/Frontiers/submission/Classification_With_Feats_Selection/Best_hyperpameters_2/GITA/RP/BAGG/RP.txt'
+
+import sys
+sys.path.append("/export/b15/afavaro/git_code_version/speech")
+from Cross_Lingual_Evaluation.interpretable_features.classification.mono_lingual.Data_Prep_RP import *
+from Cross_Lingual_Evaluation.interpretable_features.classification.mono_lingual.Utils import *
+import random
+import os
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedStratifiedKFold
+random.seed(10)
+
+spain_data = gita_prep(os.path.join(BASE, "GITA/total_data_frame_novel_task_combined_ling_tot.csv"))
+gr = spain_data.groupby('labels')
+ctrl_ = gr.get_group(0)
+pd_ = gr.get_group(1)
+
+arrayOfSpeaker_cn = ctrl_['names'].unique()
+random.shuffle(arrayOfSpeaker_cn)
+arrayOfSpeaker_pd = pd_['names'].unique()
+random.shuffle(arrayOfSpeaker_pd)
+
+cn_sps = get_n_folds(arrayOfSpeaker_cn)
+pd_sps = get_n_folds(arrayOfSpeaker_pd)
+
+data = []
+for cn_sp, pd_sp in zip(sorted(cn_sps, key=len), sorted(pd_sps, key=len, reverse=True)):
+    data.append(cn_sp + pd_sp)
+n_folds = sorted(data, key=len, reverse=True)
+
+folds = []
+for i in n_folds:
+    data_i = spain_data[spain_data["names"].isin(i)]
+    data_i = data_i.drop(columns=['names', 'AudioFile', 'task'])
+    folds.append(data_i.to_numpy())
+
+data_train_1, data_test_1, data_train_2, data_test_2, data_train_3, data_test_3, data_train_4, data_test_4, \
+data_train_5, data_test_5,  data_train_6, data_test_6, data_train_7, data_test_7 , data_train_8, data_test_8, \
+data_train_9, data_test_9, data_train_10, data_test_10 = create_split_train_test(folds)
+
+####################################################################################################################################
+
+svm_parameters = {}
+rf_paramters = {}
+knn_paramters = {}
+xg_paramters = {}
+bagg_paramters = {}
+
+
+for i in range(1, 11):
+
+    print(i)
+
+    normalized_train_X, normalized_test_X, y_train, y_test = normalize(eval(f"data_train_{i}"),
+                                                                                       eval(f"data_test_{i}"))
+    clf = ExtraTreesClassifier(n_estimators=50)
+    clf = clf.fit(normalized_train_X, y_train)
+    model = SelectFromModel(clf, prefit=True, max_features=30)
+    X_train = model.transform(normalized_train_X)
+    cols = model.get_support(indices=True)
+
+    X_test = normalized_test_X[:, cols]
+    reduced_data = data_i.iloc[:, :-1]
+    selected_features = reduced_data.columns[model.get_support()].to_list()
+
+    model = SVC()
+    kernel = ['poly', 'rbf', 'sigmoid']
+    C = [50, 10, 1.0, 0.1, 0.01]
+    gamma = [1, 0.1, 0.01, 0.001]
+    # gamma = ['scale']
+    # define grid search
+    grid = dict(kernel=kernel, C=C, gamma=gamma)
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+    grid_result = grid_search.fit(normalized_train_X, y_train)
+    print(grid_result.best_params_)
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, config in zip(means, params):
+        config = str(config)
+        if config in svm_parameters:
+            svm_parameters[config].append(mean)
+        else:
+            svm_parameters[config] = [mean]
+
+    # KNN
+    model = KNeighborsClassifier()
+    n_neighbors = range(1, 21, 2)
+    weights = ['uniform', 'distance']
+    metric = ['euclidean', 'manhattan', 'minkowski']
+    # define grid search
+    grid = dict(n_neighbors=n_neighbors, weights=weights, metric=metric)
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+    grid_result = grid_search.fit(normalized_train_X, y_train)
+    print(grid_result.best_params_)
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, config in zip(means, params):
+        config = str(config)
+        if config in knn_paramters:
+            knn_paramters[config].append(mean)
+        else:
+            knn_paramters[config] = [mean]
+
+    # Random Forest
+
+    model = RandomForestClassifier()
+    n_estimators = [10, 100, 1000]
+    max_features = ['sqrt', 'log2']
+    # define grid search
+    grid = dict(n_estimators=n_estimators, max_features=max_features)
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+    grid_result = grid_search.fit(normalized_train_X, y_train)
+    print(grid_result.best_params_)
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, config in zip(means, params):
+        config = str(config)
+        if config in rf_paramters:
+            rf_paramters[config].append(mean)
+        else:
+            rf_paramters[config] = [mean]
+
+    # Gradient Boost Classifier
+    model = GradientBoostingClassifier()
+    n_estimators = [10, 100, 1000]
+    learning_rate = [0.001, 0.01, 0.1]
+    subsample = [0.5, 0.7, 1.0]
+    max_depth = [3, 7, 9]
+    # define grid search
+    grid = dict(learning_rate=learning_rate, n_estimators=n_estimators, subsample=subsample, max_depth=max_depth)
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+    grid_result = grid_search.fit(normalized_train_X, y_train)
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+
+    for mean, config in zip(means, params):
+        config = str(config)
+        if config in xg_paramters:
+            xg_paramters[config].append(mean)
+        else:
+            xg_paramters[config] = [mean]
+
+    # Bagging Classifier
+    model = BaggingClassifier()
+    max_samples = [0.05, 0.1, 0.2, 0.5]
+    n_estimators = [10, 100, 1000]
+    # define grid search
+    grid = dict(n_estimators=n_estimators, max_samples=max_samples)
+    cv = RepeatedStratifiedKFold(n_splits=9, n_repeats=3, random_state=1)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='accuracy', error_score=0)
+    grid_result = grid_search.fit(normalized_train_X, y_train)
+    print(grid_result.best_params_)
+
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+
+    for mean, config in zip(means, params):
+        config = str(config)
+        if config in bagg_paramters:
+            bagg_paramters[config].append(mean)
+        else:
+            bagg_paramters[config] = [mean]
+
+
+for k in svm_parameters.keys():
+    svm_parameters[k] = np.array(svm_parameters[k]).mean()
+
+for k in knn_paramters.keys():
+    knn_paramters[k] = np.array(knn_paramters[k]).mean()
+
+for k in rf_paramters.keys():
+    rf_paramters[k] = np.array(rf_paramters[k]).mean()
+
+for k in xg_paramters.keys():
+    xg_paramters[k] = np.array(xg_paramters[k]).mean()
+
+for k in bagg_paramters.keys():
+    bagg_paramters[k] = np.array(bagg_paramters[k]).mean()
+
+fo = open(SVM_OUT_PATH, "w")
+for k, v in svm_parameters.items():
+    fo.write(str(k) + ' >>> '+ str(v) + '\n\n')
+
+fo = open(KNN_OUT_PATH, "w")
+for k, v in knn_paramters.items():
+    fo.write(str(k) + ' >>> '+ str(v) + '\n\n')
+
+fo = open(RF_OUT_PATH, "w")
+for k, v in rf_paramters.items():
+    fo.write(str(k) + ' >>> '+ str(v) + '\n\n')
+
+fo = open(XG_OUT_PATH, "w")
+for k, v in xg_paramters.items():
+    fo.write(str(k) + ' >>> '+ str(v) + '\n\n')
+
+fo = open(BAGG_OUT_PATH, "w")
+for k, v in bagg_paramters.items():
+    fo.write(str(k) + ' >>> ' + str(v) + '\n\n')
+
