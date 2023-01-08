3b6f6f1ce9b81f0a04d83565fd428700ccaeb8d5
annafavaro <anna.favaro96@gmail.com>
2023-01-08 09:18:05 -0500

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
@@ -4,7 +4,6 @@ base = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k'
 # output directory where to save speech transcripts.
 output_folder = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_Transcripts/'
 
-
 import os
 import whisper
 
