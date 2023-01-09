f3bca608410fd3a826f5ca68ba942b1879d7cde4
annafavaro <anna.favaro96@gmail.com>
2023-01-09 11:16:41 -0500

    first

--- Multilingual_Evaluation/data_preprocessing/convert_to_16k.sh
+++ Multilingual_Evaluation/data_preprocessing/convert_to_16k.sh
@@ -1,8 +1,9 @@
-in_dir=/export/c12/afavaro/NLS_Data_Set
-out_dir=/export/b15/afavaro/Frontiers/NLS/NLS_RESAMPLED/
-
+# Resample directory containing audio files to 16 kHZ using sox.
 # in_dir: directory containing recordings to be converted
 # out_dir: output directory containing recordings converted to 16 kHz.
+
+in_dir=/export/c12/afavaro/NLS_Data_Set
+out_dir=/export/b15/afavaro/Frontiers/NLS/NLS_RESAMPLED/
 
 for audio_File in $in_dir/*.wav
 do
