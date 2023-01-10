80617d8abf2903c8711d7140a6ff978e21b5cc4a
annafavaro <anna.favaro96@gmail.com>
2023-01-10 07:48:55 -0500

    first

--- Cross_Lingual_Evaluation/interpretable_features/feature_extraction/whisperix_aligner_english_all.py
+++ Cross_Lingual_Evaluation/interpretable_features/feature_extraction/whisperix_aligner_english_all.py
@@ -17,7 +17,7 @@ for m in audios:
     if size > 56:
         files.append(m)
 
-indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/NLS_095_ses01_WordColor.wav")
+indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/NLS_003_ses02_Bluered.wav")
 
 for audio in files[indx+1:]:
     text =[]
