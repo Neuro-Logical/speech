76e2e6f53aa14a40fc247c6b6d16c96f93ed4024
TsaiYT <yitingtsai0213@gmail.com>
2023-01-11 14:55:20 -0500

    Add requirements and modified readme and the experiment script
    - add requirements.txt
    - modified readme
    - modified the experiment script

--- Cross_Lingual_Evaluation/Non_Interpretable_features/README.md
+++ Cross_Lingual_Evaluation/Non_Interpretable_features/README.md
@@ -1,4 +1,7 @@
 ## Non-interpretable approaches: x-vectors and TRILLsson
-To run the code, the SpeechBrain toolkit is required.
+Before running the code, install the dependencies from `requirements.txt`
 
-The whole experiment pipeline can be run through `run_all_experiments.sh`. Please set the paths of data set directory, feature directory, and output result directory before running. The feature folder needs to contain sub-folders *xvector* and *trill*. Access to the JHU CLSP grid is required to run the experiments with the data sets we experimented on. All experiment output results will be saved in the *output* directory.
+The whole experiment pipeline can be run through `run_all_experiments.sh`. X-vectors and TRILLsson representations are first extracted and saved in the feature directory. After that, mono-lingual, multi-lingual, and cross-lingual experiments will be run.
+
+Please set the paths of feature directory and output result directory before running the script. 
+The feature folder needs to contain sub-folders *xvector* and *trill*. Access to the JHU CLSP grid is required to run the experiments with the data sets and data partitions that match the ones we used in the interpretable approaches. All experiment output results will be saved in the *output* directory.
--- Cross_Lingual_Evaluation/Non_Interpretable_features/run_all_experiments.sh
+++ Cross_Lingual_Evaluation/Non_Interpretable_features/run_all_experiments.sh
@@ -1,46 +1,47 @@
 #!/usr/bin/env bash
 
-
-# change to the full path of where the Non_Interpretable_features folder is
+# change this to the full path of where the Non_Interpretable_features folder is
 cd '/home/ytsai25/Non_Interpretable_features'
 
 # set folder to save feature files (need to have subfolders "xvector" and "trill")
 feat_pth='/export/b11/ytsai25/feats/'
-# set input folder that contains all data sets
-rec_pth='/export/b15/afavaro/Frontiers/'
 # set folder to save printed experiment results
 out_pth='/home/ytsai25/Non_Interpretable_features/output/'
+mkdir -p output
+# input folder that contains all data sets
+rec_pth='/export/b15/afavaro/Frontiers/'
 
-# feature extraction (uncomment this part if needed)
+# feature extraction (features already extracted and saved on the grid, uncomment this part if to re-extract)
 # python /feature_extraction/feature_extraction.py $rec_pth $feat_pth
 
-# mono-lingual experiments
+# mono-lingual experiments ==================
+# Colombian Spanish
 python nested_cross_validation/monolingual/Colombian/TDU.py $feat_pth > "${out_pth}output_mono_gita_tdu.txt"
 python nested_cross_validation/monolingual/Colombian/SS.py $feat_pth > "${out_pth}output_mono_gita_monologue.txt"
 python nested_cross_validation/monolingual/Colombian/RP.py $feat_pth > "${out_pth}output_mono_gita_readpassage.txt"
-
+# Czech
 python nested_cross_validation/monolingual/Czech/RP.py $feat_pth > "${out_pth}output_mono_czech_readpassage.txt"
 python nested_cross_validation/monolingual/Czech/SS.py $feat_pth > "${out_pth}output_mono_czech_monologue.txt"
-
+# German
 python nested_cross_validation/monolingual/German/TDU.py $feat_pth > "${out_pth}output_mono_ger_tdu.txt"
 python nested_cross_validation/monolingual/German/SS.py $feat_pth > "${out_pth}output_mono_ger_monologue.txt"
 python nested_cross_validation/monolingual/German/RP.py $feat_pth > "${out_pth}output_mono_ger_readpassage.txt"
-
+# Italian
 python nested_cross_validation/monolingual/Italian/TDU.py $feat_pth > "${out_pth}output_mono_ita_tdu.txt"
 python nested_cross_validation/monolingual/Italian/RP.py $feat_pth > "${out_pth}output_mono_ita_readpassage.txt"
-
+# English
 python nested_cross_validation/monolingual/NLS/RP.py $feat_pth > "${out_pth}output_mono_nls_readpassage.txt"
 python nested_cross_validation/monolingual/NLS/SS.py $feat_pth > "${out_pth}output_mono_nls_monologue.txt"
-
+# Castilian spanish
 python nested_cross_validation/monolingual/Spanish/TDU.py $feat_pth > "${out_pth}output_mono_neu_tdu.txt"
 python nested_cross_validation/monolingual/Spanish/SS.py $feat_pth > "${out_pth}output_mono_neu_monologue.txt"
 
-# multi-lingual experiments
+# multi-lingual experiments ==================
 python nested_cross_validation/multilingual/TDU.py $feat_pth > "${out_pth}output_multi_tdu.txt"
 python nested_cross_validation/multilingual/SS.py $feat_pth > "${out_pth}output_multi_monologue.txt"
 python nested_cross_validation/multilingual/RP.py $feat_pth > "${out_pth}output_multi_readpassage.txt"
 
-# cross-lingual experiments
+# cross-lingual experiments ==================
 python nested_cross_validation/crosslingual/TDU.py $feat_pth > "${out_pth}output_cross_tdu.txt"
 python nested_cross_validation/crosslingual/SS.py $feat_pth > "${out_pth}output_cross_monologue.txt"
 python nested_cross_validation/crosslingual/RP.py $feat_pth > "${out_pth}output_cross_readpassage.txt"
