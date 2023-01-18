ec870f5766a43260bcfdf0174f8c63db4ef96d10
annafavaro <anna.favaro96@gmail.com>
2023-01-18 07:23:27 -0500

    first

---
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_AD.py
@@ -0,0 +1,31 @@
+
+output_folder = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_Transcripts/'
+
+import os
+import whisper
+
+tot = "/export/b16/afavaro/AD_speech/"
+tots =[elem.split(".wav")[0] for elem in os.listdir(tot)]
+tot_1 = "/export/b16/afavaro/Transcripts/"
+tots_1 =[elem.split(".txt")[0] for elem in os.listdir(tot_1)]
+files = (set(tots)^set(tots_1))
+files = [os.path.join(tot, file + ".wav") for file in files]
+
+files_new = []
+for m in files:
+    size = os.stat(m).st_size / 1000
+    if size > 56:
+        if "Cookie" in m:
+            files_new.append(m)
+
+# extract and save transcripts in text files.
+for i in files[files_new]:
+    print(i)
+    model = whisper.load_model("medium")
+    result = model.transcribe(i)
+    test = result['text']
+    base = os.path.basename(i).split(".wav")[0]
+    total = os.path.join(output_folder, base + ".txt")
+    text_file = open(total, "wt")
+    n = text_file.write(test)
+    text_file.close()
