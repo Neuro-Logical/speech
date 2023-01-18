d7896d21a5d291d7f254107be9b11ff1cbe34b1f
annafavaro <anna.favaro96@gmail.com>
2023-01-18 07:11:41 -0500

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
@@ -16,8 +16,10 @@ for m in paths:
         if "NLS_125" in m:
             files.append(m)
 
+indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/NLS_125_ses01_WordColor.wav")
+
 # extract and save transcripts in text files.
-for i in files:
+for i in files[indx:]:
     print(i)
     model = whisper.load_model("medium")
     result = model.transcribe(i)
