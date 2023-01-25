d66dc163142f65981e87ce9f28ac0c98b2eecc9b
annafavaro <anna.favaro96@gmail.com>
2023-01-25 16:38:31 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/whisperix_aligner_english_all.py
+++ Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/whisperix_aligner_english_all.py
@@ -21,7 +21,7 @@ for m in audios:
 
 indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/PEC_019_ses01_SmoothSustained5.wav")
 
-for audio in files[indx:]:
+for audio in files[indx+1:]:
     print(audio)
     text =[]
     time_stamps = []
