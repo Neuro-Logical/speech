bbd5326874cf253b4935c9038a5038f3c8af3618
annafavaro <anna.favaro96@gmail.com>
2023-04-12 16:02:19 -0400

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
@@ -11,14 +11,36 @@ files = []
 for m in paths:
     size = os.stat(m).st_size / 1000
     if size > 56:
-        if "Cookie" in m:
+        if "NLS_018_ses01_CookieThief" in m:
+            files.append(m)
+        if "NLS_049_ses01_CookieThief" in m:
+            files.append(m)
+        if "PEC_027_ses01_CookieThief" in m:
+            files.append(m)
+        if "PEC_039_ses01_CookieThief" in m:
+            files.append(m)
+        if "PEC_048_ses01_CookieThief" in m:
+            files.append(m)
+        if "PEC_067_ses01_CookieThief" in m:
+            files.append(m)
+        if "NLS_015_ses01_CookieThief'" in m:
+            files.append(m)
+        if "NLS_035_ses01_CookieThief" in m:
             files.append(m)
 
-indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/PEC_063_ses01_CookieThief.wav")
+        if "AD_007_ses03_CookieThief" in m:
+            files.append(m)
+        if "PEC_065_ses01_CookieThief" in m:
+            files.append(m)
+        if "NLS_014_ses01_CookieThief" in m:
+            files.append(m)
+        if "NLS_050_ses01_CookieThief" in m:
+            files.append(m)
+
+#indx = files.index("/export/c12/afavaro/New_NLS/NLS_Speech_Data_All_16k/PEC_063_ses01_CookieThief.wav")
 
 # extract and save transcripts in text files.
-#for i in files[indx+2:]:
-for i in files[indx+1:]:
+for i in files:
     print(i)
     model = whisper.load_model("medium")
     result = model.transcribe(i)
