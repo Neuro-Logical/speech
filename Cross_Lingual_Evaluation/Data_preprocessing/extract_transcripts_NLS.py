d8428b1d0da9821aa376906d76135c17c1db4fcd
annafavaro <anna.favaro96@gmail.com>
2023-04-21 17:37:44 -0400

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
@@ -1,5 +1,5 @@
 # base input directory containing recordings to transcribe.
-base = '/export/c12/afavaro/New_NLS/audio_fusion_new/all_audios/'
+base = '/export/c12/afavaro/New_NLS/audio_fusion_new/ctp_audios'
 # output directory where to save speech transcripts.
 output_folder = '/export/c12/afavaro/New_NLS/audio_fusion_new/all_audio_transcripts/'
 
