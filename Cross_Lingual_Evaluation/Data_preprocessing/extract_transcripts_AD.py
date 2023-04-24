64a0c2d07275d8fdb0a26d0332abe928ff78ec21
annafavaro <anna.favaro96@gmail.com>
2023-04-24 19:45:57 -0400

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_AD.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_AD.py
@@ -3,23 +3,20 @@
 
 import os
 import whisper
-
-tot = "/export/b16/afavaro/AD_speech/"
-tots =[elem.split(".wav")[0] for elem in os.listdir(tot)]
-tot_1 = "/export/b16/afavaro/Transcripts/"
-tots_1 =[elem.split(".txt")[0] for elem in os.listdir(tot_1)]
-files = (set(tots)^set(tots_1))
-files = [os.path.join(tot, file + ".wav") for file in files]
+base = '/export/c07/afavaro/KCL_PD_DATA/KCL_PD_Dataset/ReadText/all_audio_resampled/'
+# output directory where to save speech transcripts.
+output_folder = '/export/c07/afavaro/KCL_PD_DATA/KCL_PD_Dataset/ReadText/all_audios_transcripts/'
+path_audiods = [os.path.join(base, elem) for elem in os.listdir(base)]
 
 files_new = []
-for m in files:
+for m in path_audiods:
     size = os.stat(m).st_size / 1000
     if size > 56:
-        if "Cookie" in m:
             files_new.append(m)
 print(len(files_new))
+
 # extract and save transcripts in text files.
-for i in files_new:
+for i in files_new[-20]:
     print(i)
     model = whisper.load_model("medium")
     result = model.transcribe(i)
