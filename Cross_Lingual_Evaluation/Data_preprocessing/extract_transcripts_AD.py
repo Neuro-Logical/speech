6a8e09e067a045fb74ffe4ca41090f2dce8454b9
annafavaro <anna.favaro96@gmail.com>
2023-05-03 12:05:14 -0400

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_AD.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_AD.py
@@ -1,7 +1,7 @@
 # base input directory containing recordings to transcribe.
-base = '/export/c07/afavaro/KCL_PD_DATA/KCL_PD_Dataset/ReadText/all_audios_transcripts/to_refine/'
+base = '/export/c12/afavaro/Phonological model/all_recordings/'
 # output directory where to save speech transcripts.
-output_folder = '/export/c07/afavaro/KCL_PD_DATA/KCL_PD_Dataset/ReadText/all_audios_transcripts/'
+output_folder = '/export/c12/afavaro/Phonological model/all_transcripts/'
 
 import os
 import whisper
@@ -9,30 +9,28 @@ print("CIAOAOO")
 
 
 path_audiods = [os.path.join(base, elem) for elem in os.listdir(base)]
-#path_tr = [os.path.join(output_folder, elem) for elem in os.listdir(output_folder)]
+path_tr = [os.path.join(output_folder, elem) for elem in os.listdir(output_folder)]
 ##
-#names_tr = [os.path.basename(elem).split(".txt")[0] for elem in path_tr]
-#names_audio = [os.path.basename(elem).split(".wav")[0] for elem in path_audiods]
+names_tr = [os.path.basename(elem).split(".txt")[0] for elem in path_tr]
+names_audio = [os.path.basename(elem).split(".wav")[0] for elem in path_audiods]
 ##
-#all_names = list(set(names_tr) ^ set(names_audio))
-#all_names_complete = [os.path.join(base, elem + ".wav") for elem in all_names]
+all_names = list(set(names_tr) ^ set(names_audio))
+all_names_complete = [os.path.join(base, elem + ".wav") for elem in all_names]
 #
 #
-files = []
-for m in path_audiods:
-    size = os.stat(m).st_size / 1000
-    if size > 56:
+#files = []
+#for m in path_audiods:
+   # size = os.stat(m).st_size / 1000
+   # if size > 56:
        # if "CookieThief" in m:
-            files.append(m)
+           # files.append(m)
 
       #  if "NLS_082_ses01_CookieThief" in m:
 
-print(len(files))
+print(len(all_names_complete))
 print("done")
-#indx = files.index("/export/c12/afavaro/New_NLS/audio_fusion_new/all_audios/PEC_028_ses01_CookieThief.wav")
-
 # extract and save transcripts in text files.
-for i in files[-6:]:
+for i in all_names_complete:
     print(i)
     model = whisper.load_model("base")
     result = model.transcribe(i)
