ac60739e7a361064a72c10430ae86a96da56afde
annafavaro <anna.favaro96@gmail.com>
2023-05-21 10:15:06 -0400

    first

--- Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/compute_intensity.py
+++ Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/compute_intensity.py
@@ -7,10 +7,10 @@ import parselmouth
 from feature_extraction_utils import *
 
 name = 'intensity_all_audios'
-gita = '/export/c12/afavaro/Phonological_model/all_audios_laureano/'
+gita = '/export/b16/afavaro/AD_longitudinal_may/audios/'
 files = [os.path.join(gita, elem) for elem in sorted(os.listdir(gita))]
 
-out_path = '/export/c12/afavaro/Phonological_model/all_feats_laureano/'
+out_path = '/export/b16/afavaro/AD_longitudinal_may/feats/'
 out_file = os.path.join(out_path, name + ".csv")
 print(out_file)
 
