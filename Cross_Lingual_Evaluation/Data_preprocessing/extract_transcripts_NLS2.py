bfecddb0e3b2d2e140d8ff1e2e67ebf3e71e9b42
annafavaro <anna.favaro96@gmail.com>
2023-09-23 23:34:38 -0400

    first

---
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS2.py
@@ -0,0 +1,23 @@
+# base input directory containing recordings to transcribe.
+base = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/data/after_10_above/'
+# output directory where to save speech transcripts.
+output_folder = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/transcripts/after_10_above/'
+
+import os
+import whisper
+
+path_audios = [os.path.join(base, elem) for elem in os.listdir(base)]
+
+print(len(path_audios))
+# extract and save transcripts in text files.
+for num, i in enumerate(path_audios):
+    print(num)
+    print(i)
+    model = whisper.load_model("base")
+    result = model.transcribe(i)
+    test = result['text']
+    base = os.path.basename(i).split(".wav")[0]
+    total = os.path.join(output_folder, base + ".txt")
+    text_file = open(total, "wt")
+    n = text_file.write(test)
+    text_file.close()
---
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS3.py
@@ -0,0 +1,23 @@
+# base input directory containing recordings to transcribe.
+base = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/data/5_before/'
+# output directory where to save speech transcripts.
+output_folder = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/transcripts/5_before/'
+
+import os
+import whisper
+
+path_audios = [os.path.join(base, elem) for elem in os.listdir(base)]
+
+print(len(path_audios))
+# extract and save transcripts in text files.
+for num, i in enumerate(path_audios):
+    print(num)
+    print(i)
+    model = whisper.load_model("base")
+    result = model.transcribe(i)
+    test = result['text']
+    base = os.path.basename(i).split(".wav")[0]
+    total = os.path.join(output_folder, base + ".txt")
+    text_file = open(total, "wt")
+    n = text_file.write(test)
+    text_file.close()
