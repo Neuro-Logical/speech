3cf27caa3d609c1e9c9ed19f9e38c4a19e1a31d0
annafavaro <anna.favaro96@gmail.com>
2023-09-23 23:31:58 -0400

    first

--- Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
+++ Cross_Lingual_Evaluation/Data_preprocessing/extract_transcripts_NLS.py
@@ -1,36 +1,19 @@
 # base input directory containing recordings to transcribe.
-base = '/scratch4/lmorove1/afavaro/data/New_NLS/NLS_Speech_Data_All_Files_16k/'
+base = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/data/after_10/'
 # output directory where to save speech transcripts.
-output_folder = '/scratch4/lmorove1/afavaro/data/New_NLS/NLS_Speech_Data_Transcripts_RockFish/'
+output_folder = '/export/b16/afavaro/PARKCELEB/experiments_english3/longitudinal_study/transcripts/10_after/'
 
 import os
 import whisper
-print("CIAOAOO")
 
+path_audios = [os.path.join(base, elem) for elem in os.listdir(base)]
 
-path_audiods = [os.path.join(base, elem) for elem in os.listdir(base)]
-#path_tr = [os.path.join(output_folder, elem) for elem in os.listdir(output_folder)]
-#
-#names_tr = [os.path.basename(elem).split(".txt")[0] for elem in path_tr]
-#names_audio = [os.path.basename(elem).split(".wav")[0] for elem in path_audiods]
-#
-#all_names = list(set(names_tr) ^ set(names_audio))
-#all_names_complete = [os.path.join(base, elem + ".wav") for elem in all_names]
-
-
-files = []
-for m in path_audiods:
-    size = os.stat(m).st_size / 1000
-    if size > 56:
-        files.append(m)
-
-print(len(files))
-print("done")
-#indx = files.index("/export/c12/afavaro/New_NLS/audio_fusion_new/all_audios/PEC_028_ses01_CookieThief.wav")
-
-for i in files:
+print(len(path_audios))
+# extract and save transcripts in text files.
+for num, i in enumerate(path_audios):
+    print(num)
     print(i)
-    model = whisper.load_model("medium")
+    model = whisper.load_model("base")
     result = model.transcribe(i)
     test = result['text']
     base = os.path.basename(i).split(".wav")[0]
