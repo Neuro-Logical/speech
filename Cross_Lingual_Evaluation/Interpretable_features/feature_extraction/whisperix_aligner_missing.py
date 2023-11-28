d6f88ed484c7657eda168b17be3c3330c3bb5423
annafavaro <anna.favaro96@gmail.com>
2023-11-28 11:15:26 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/whisperix_aligner_missing.py
+++ Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/whisperix_aligner_missing.py
@@ -8,7 +8,7 @@ import pandas as pd
 device = "cpu"
 model = whisperx.load_model("small", device)
 
-one = '/export/b01/afavaro/tmeyer_alignment/audio_all/'
+one = '/export/b01/afavaro/tmeyer_new_output/tmeyer_resampled_audios/'
 files = [os.path.join(one, elem) for elem in os.listdir(one)]
 #tots = [elem.split(".wav")[0] for elem in os.listdir(one)]
 #two = '/export/c12/afavaro/New_NLS/NLS_Speech_Data_Word_Alignment_whisperx'
