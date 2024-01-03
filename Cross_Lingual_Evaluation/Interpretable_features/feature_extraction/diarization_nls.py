de00cb3c0aa5e678c626824d068f23a10077d928
annafavaro <anna.favaro96@gmail.com>
2024-01-03 07:23:46 -0500

    first

--- Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/diarization_nls.py
+++ Cross_Lingual_Evaluation/Interpretable_features/feature_extraction/diarization_nls.py
@@ -21,7 +21,7 @@ for m in files:
             files_new.append(m)
 
 
-path = os.path.join(one, 'NLS_047_ses01_Namingaction1.wav')
+path = os.path.join(one, 'AD_003_ses03_Namingaction10.wav')
 ind = files_new.index(path)
 
 for audio_file in files_new[ind+1:]:
